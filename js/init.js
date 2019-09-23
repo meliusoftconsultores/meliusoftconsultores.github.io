fbef41c44ada7ecd4c8ccace8b4f4ee3aec49b45
Rafa-98 <rmendeznieves98@gmail.com>
2019-09-23 08:47:16 -0400

    Version 1.1

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();      
+      M.textareaAutoResize($('#textarea1'));
+  
+    }); 
+  })(jQuery);
